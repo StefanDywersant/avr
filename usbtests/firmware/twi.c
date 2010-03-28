3341fa2a1be4bc4307a9a4325f527ad8fc7cc16c
kharg <kharg@4033daf9-1f9f-41a4-b45b-5cb3f2ea6f27>
2010-03-28 22:42:56 +0000

    biblioteka pcf8583 i twi

---
+++ usbtests/firmware/twi.c
@@ -0,0 +1,138 @@
+#include "twi.h"
+#include <util/twi.h>
+
+// Private methods
+
+void twiWriteStart() {
+	TWCR = (1 << TWSTA) | (1 << TWINT) | (1 << TWEN);
+}
+
+void twiWaitForBus() {
+	while (!(TWCR & (1 << TWINT)));
+}
+
+void twiWriteStop() {
+	TWCR = (1 << TWSTO) | (1 << TWINT) | (1 << TWEN);
+}
+
+void twiWriteSlaveAddress(uint8_t address, uint8_t mode) {
+	TWDR = (address & 0xf0) | mode;
+	TWCR = (1 << TWINT) | (1 << TWEN);
+}
+
+void twiWriteDataByte(uint8_t byte) {
+	TWDR = byte;
+	TWCR = (1 << TWINT) | (1 << TWEN);
+}
+
+void twiReadDataByte(uint8_t *byte, uint8_t ack) {
+	if (ack)
+		TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
+	else
+		TWCR = (1 << TWINT) | (1 << TWEN);
+
+	twiWaitForBus();
+
+	*byte = TWDR;
+}
+
+// Public methods
+
+void twiInit(uint32_t bitrate) {
+	// clear status register
+	TWSR = 0x00;
+
+	// set bitrate
+	TWBR = ((F_CPU / bitrate) - 16) / 2;
+}
+
+uint8_t twiSyncMTMR(uint8_t address, uint8_t *requestData, uint8_t requestLen, uint8_t *responseData, uint8_t responseLen) {
+
+/*	while (requestLen--)
+		requestData++;
+
+	while (responseLen--)
+		twiReadDataByte(responseData++);
+
+	return TWI_OK;*/
+
+	// Master Transmitter
+
+	// write start condition
+	twiWriteStart();
+	twiWaitForBus();
+
+	if (TW_STATUS != TW_START) {
+		twiWriteStop();
+		return 0xfe;
+//		return TWI_ERROR;
+	}
+
+	// write SLA+W
+	twiWriteSlaveAddress(address, TW_WRITE);
+	twiWaitForBus();
+
+	if (TW_STATUS != TW_MT_SLA_ACK) {
+		twiWriteStop();
+		return 0xfd;
+//		return TWI_ERROR;
+	}
+
+	// write request data
+	while (requestLen--) {
+		twiWriteDataByte(*requestData++);
+		twiWaitForBus();
+
+		if (TW_STATUS != TW_MT_DATA_ACK) {
+			twiWriteStop();
+			return 0xfc;
+	//		return TWI_ERROR;
+		}
+	}
+
+	// Master Receiver
+
+	// write repeated start
+	twiWriteStart();
+	twiWaitForBus();
+
+	if (TW_STATUS != TW_REP_START) {
+		twiWriteStop();
+		return TWI_ERROR;
+	}
+
+	// write SLA+R
+	twiWriteSlaveAddress(address, TW_READ);
+	twiWaitForBus();
+
+	if (TW_STATUS != TW_MR_SLA_ACK) {
+		twiWriteStop();
+		return TWI_ERROR;
+	}
+
+	// read data
+	while (responseLen > 1) {
+		twiReadDataByte(responseData++, 1);
+		twiWaitForBus();
+
+		if (TW_STATUS != TW_MR_DATA_ACK) {
+			twiWriteStop();
+			return TWI_ERROR;
+		}
+
+		responseLen--;
+	}
+
+	twiReadDataByte(responseData++, 0);
+	twiWaitForBus();
+
+	if (TW_STATUS != TW_MR_DATA_NACK) {
+		twiWriteStop();
+		return TWI_ERROR;
+	}
+
+	// write stop condition
+	twiWriteStop();
+
+	return TWI_OK;
+}
