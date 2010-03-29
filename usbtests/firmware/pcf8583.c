3fd00098f1fe3fa6df82c6383ddbcbbdb9e53733
kharg <kharg@4033daf9-1f9f-41a4-b45b-5cb3f2ea6f27>
2010-03-29 21:21:03 +0000

    wysyłanie czasu przez usb

--- usbtests/firmware/pcf8583.c
+++ usbtests/firmware/pcf8583.c
@@ -14,37 +14,32 @@
 #define PCF8583_ADDR_MINUTE 0x03
 #define PCF8583_ADDR_HOUR 0x04
 
-uint8_t pcf8583GetTime(uint8_t addr, pcf8583TimeStruct** time) {
+uint8_t pcf8583GetTime(uint8_t addr, pcf8583TimeStruct* time) {
 	uint8_t type;
-	pcf8583TimeStruct* _time;
-
-	_time = malloc(sizeof(pcf8583TimeStruct));
 
 	// milisecond
 	type = PCF8583_ADDR_MILISECOND;
-	if (twiSyncMTMR(addr, &type, 1, &_time->milisecond, 1) != TWI_OK)
+	if (twiSyncMTMR(addr, &type, 1, &time->milisecond, 1) != TWI_OK)
 		return 0xfe;
-	_time->milisecond = (_time->milisecond >> 4) * 0x0a + (_time->milisecond & 0x0f);
+	time->milisecond = (time->milisecond >> 4) * 0x0a + (time->milisecond & 0x0f);
 
 	// second
 	type = PCF8583_ADDR_SECOND;
-	if (twiSyncMTMR(addr, &type, 1, &_time->second, 1) != TWI_OK)
+	if (twiSyncMTMR(addr, &type, 1, &time->second, 1) != TWI_OK)
 		return 0xfd;
-	_time->second = (_time->second >> 4) * 0x0a + (_time->second & 0x0f);
+	time->second = (time->second >> 4) * 0x0a + (time->second & 0x0f);
 
 	// minute
 	type = PCF8583_ADDR_MINUTE;
-	if (twiSyncMTMR(addr, &type, 1, &_time->minute, 1) != TWI_OK)
+	if (twiSyncMTMR(addr, &type, 1, &time->minute, 1) != TWI_OK)
 		return PCF8583_ERROR;
-	_time->minute = (_time->minute >> 4) * 0x0a + (_time->minute & 0x0f);
+	time->minute = (time->minute >> 4) * 0x0a + (time->minute & 0x0f);
 
 	// hour
 	type = PCF8583_ADDR_HOUR;
-	if (twiSyncMTMR(addr, &type, 1, &_time->hour, 1) != TWI_OK)
+	if (twiSyncMTMR(addr, &type, 1, &time->hour, 1) != TWI_OK)
 		return PCF8583_ERROR;
-	_time->hour = (_time->hour >> 4) * 0x0a + (_time->hour & 0x0f);
-
-	*time = _time;
+	time->hour = (time->hour >> 4) * 0x0a + (time->hour & 0x0f);
 
 	return PCF8583_OK;
 }
