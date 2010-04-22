9bc4df38a86a46d2f2aea1723508e3b297cfa80b
kharg <karol@maciaszek.pl>
2010-04-22 17:49:58 +0000

    - spi

--- usbtests/firmware/spi.h
+++ usbtests/firmware/spi.h
@@ -10,13 +10,22 @@
 #ifndef SPI_H_
 #define SPI_H_
 
-#define SPI_MISO_PIN 0
-#define SPI_SCK_PIN 1
-#define SPI_MOSI_PIN 2
-#define SPI_CSN_PIN 3
+#define SPI_PORT	A
+#define SPI_MISO_PIN	0
+#define SPI_SCK_PIN	1
+#define SPI_MOSI_PIN	2
+#define SPI_CSN_PIN	3
 
 void spiInit();
-void spiOff();
+
+void spiBegin();
+
+void spiEnd();
+
 uint8_t spiReadWriteByte(uint8_t byte);
+
+void spiReadData(uint8_t len, uint8_t* data);
+
+void spiWriteData(uint8_t len, uint8_t* data);
 
 #endif /* SPI_H_ */
