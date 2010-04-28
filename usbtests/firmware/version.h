5e50db2685df12799253ff1ae8094d0f5e2e6ee9
kharg <karol@maciaszek.pl>
2010-04-28 10:32:00 +0000

    - svn:keywords

--- usbtests/firmware/version.h
+++ usbtests/firmware/version.h
@@ -9,7 +9,7 @@
 
 #define VERSION_MAJOR			0
 #define VERSION_MINOR			1
-#define VERSION_REVISION		$Rev$
+#define VERSION_REVISION		$Header$
 #define VERSION					VERSION_MAJOR ## VERSION_MINOR ## VERSION_REVISION
 
 #endif /* VERSION_H_ */
