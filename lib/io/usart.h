31969c5612b1b804ef677c93ba46bf461fba934f
kharg <karol@maciaszek.pl>
2010-06-25 13:26:24 +0000

    dodanie katalogu z bibliotekami

---
+++ lib/io/usart.h
@@ -0,0 +1,22 @@
+/**
+ * AVR Libs
+ * Biblioteki do Atmel AVR
+ *
+ * Copyright: (c) 2010 by Karol Maciaszek
+ * E-mail   : <karol.maciaszek@gmail.com>
+ * WWW      : http://www.czystybeton.pl/
+ *
+ * $Id$
+ */
+
+
+#ifndef USART_H_
+#define USART_H_
+
+void usart_init(void);
+
+void usart_put_byte(char byte);
+
+int usart_put_char(char chr, FILE* stream);
+
+#endif /* USART_H_ */
---
+++ lib/util/debug.h
@@ -0,0 +1,29 @@
+/**
+ * AVR Libs
+ * Biblioteki do Atmel AVR
+ *
+ * Copyright: (c) 2010 by Karol Maciaszek
+ * E-mail   : <karol.maciaszek@gmail.com>
+ * WWW      : http://www.czystybeton.pl/
+ *
+ * $Id$
+ */
+
+#ifndef DEBUG_H_
+#define DEBUG_H_
+
+#include <stdio.h>
+#include <avr/pgmspace.h>
+
+#if DEBUG == 1
+
+#define PRINTF(f, ...)		{ printf_P(PSTR(f), ##__VA_ARGS__); }
+
+void debug_init(void);
+
+#else
+
+#define PRINTF(f, ...)		{ }
+
+#endif /* DEBUG */
+#endif /* DEBUG_H_ */
