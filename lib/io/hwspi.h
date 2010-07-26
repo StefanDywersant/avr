a99fae83209a422db86144c0c855b3ea8724517b
kharg <karol@maciaszek.pl>
2010-07-26 20:48:11 +0000

    hardware spi library

--- lib/io/hwspi.h
+++ lib/io/hwspi.h
@@ -16,5 +16,17 @@
 
 void hwspi_init_slave(uint8_t (*func)(uint8_t));
 
+void hwspi_init_master(void);
+
+void hwspi_begin(void);
+
+void hwspi_end(void);
+
+uint8_t hwspi_read_write_byte(uint8_t wr);
+
+void hwspi_read_data(uint8_t len, uint8_t* data);
+
+void hwspi_write_data(uint8_t len, uint8_t* data);
+
 
 #endif /* HWSPI_H_ */
